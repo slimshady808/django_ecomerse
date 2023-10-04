5541bcb448d71212df2b68e8709265d63642509b
akshay <akshay@gmail.com>
2023-10-04 11:41:07 +0530

    email

--- django_ec/urls.py
+++ django_ec/urls.py
@@ -1,18 +1,4 @@
-"""django_ec URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
@@ -23,8 +9,8 @@ from django.urls import *
 urlpatterns = [
     
 
-    path('jet/dashboard/',include('jet.dashboard.urls','jet-dashboard')),
-    path('jet/',include('jet.urls')),
+    # path('jet/dashboard/',include('jet.dashboard.urls','jet-dashboard')),
+    # path('jet/',include('jet.urls')),
   
     path('admin/', admin.site.urls),
     path('admin1/',include('admin_panel.urls')),
